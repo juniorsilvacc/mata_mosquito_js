9aa3a2736cf96fff807d94dde7239f10cd6fd681
juniorsilvacc <juniorsilvacc@hotmail.com>
2020-06-08 15:05:12 -0300

    gamer over

--- jogo.js
+++ jogo.js
@@ -19,7 +19,7 @@ function posicaoRandomica(){
 
         //console.log('elementoselecionado foi: v' +vidas)
         if(vidas > 3){
-            alert('GAME OVER')
+            window.location.href = 'fim_de_jogo.html'
         } else {
             document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png'
         }
